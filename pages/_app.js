4b7a288011d8dc796c78cffaba3192374e33dd55
Nelson <tomneo2004@gmail.com>
2023-06-12 12:13:08 +0800

    add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import localFile from "next/font/local";
+import { IBM_Plex_Sans } from "next/font/google";
+
+/** nextjs host font from google font */
+const ips = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: "500",
+  style: "normal",
+});
+
+/** local font */
+const bagel = localFile({
+  src: [
+    {
+      path: "../public/fonts/Bagel_Fat_One/BagelFatOne-Regular.ttf",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${ips.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
