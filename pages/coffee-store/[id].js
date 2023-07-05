ea0e9bf9b0494291d030ad586d1ed9fb1c492e9c
Nelson <tomneo2004@gmail.com>
2023-07-05 11:51:11 +0800

    static site generation with dynamic routes

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import coffeeStoreData from "../../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+export async function getStaticProps(staticProps) {
+  const { params } = staticProps;
+  return {
+    props: {
+      coffeeStore: coffeeStoreData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
+  const { coffeeStore } = props;
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div>
       Coffee Store Page {router.query.id}
@@ -12,6 +36,8 @@ const CoffeeStore = () => {
       <Link href="/coffee-store/dynamic" legacyBehavior>
         <a>Dynamic route</a>
       </Link>
+      <p>{coffeeStore.name}</p>
+      <p>{coffeeStore.address}</p>
     </div>
   );
 };
